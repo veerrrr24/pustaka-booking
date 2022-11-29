0204335ee9283fe6d31fc5fa9d03c8dd39031d84
LyonAgyp <liony244@gmail.com>
2022-11-29 09:27:17 +0700

    Revisiion on

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Lyon Pustaka | <?= $judul; ?></title>
+    <title>Pustaka Booking | <?= $judul; ?></title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
