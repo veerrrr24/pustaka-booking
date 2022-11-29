04fde1bb25935043c2fc21887e24087a1dc33ced
Aghyksa <liony244@gmail.com>
2022-11-29 07:33:48 +0700

    lates patch

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,7 +2,7 @@
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin">
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="anggota">
     <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-book"></i>
     </div>
@@ -21,7 +21,7 @@
         <li class="nav-item active">
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link pb-0" href="<?= base_url('admin'); ?>">
+                <a class="nav-link pb-0" href="anggota">
                     <i class="fa fa-fw fa book"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -53,6 +53,50 @@
                     <span>Data Anggota</span></a>
             </li>
         </li>
+    
+    <!-- <hr class="sidebar-divider mt-3">
+
+    <div class="sidebar-heading">
+        Transaksi
+    </div>
+        <li class="nav-item active">
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('pinjam'); ?>">
+                    <i class="fa fa-fw fa-shopping-cart"></i>
+                    <span>Data Peminjaman</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('pinjam/daftarBooking'); ?>">
+                    <i class="fa fa-fw fa-list"></i>
+                    <span>Data Booking</span></a>
+            </li>
+        </li>
+
+    <hr class="sidebar-divider mt-3">
+
+    <div class="sidebar-heading">
+        Laporan
+    </div>
+        <li class="nav-item active">
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_buku'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Data Buku</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_anggota'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Data Anggota</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_pinjam'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Peminjaman</span></a>
+            </li>
+        </li>
+
+    <hr class="sidebar-divider mt-3"> -->
+
 <!-- Sidebar Toggler (Sidebar) -->
 <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
