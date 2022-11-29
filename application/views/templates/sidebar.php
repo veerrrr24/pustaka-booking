b0492a1238970d72f26ddb68eb92c5e720769605
LyonAgyp <liony244@gmail.com>
2022-11-29 09:27:25 +0700

    revisi again

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
     <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-book"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">Lyon Pustaka</div>
+    <div class="sidebar-brand-text mx-3">Pustaka Booking</div>
 </a>
 
 <!-- Divider -->
